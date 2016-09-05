01ab12f48f644e4b79f43f09a64e03c6fa89acd4
JrodManU <jrodmanu@yahoo.com>
2016-09-05 14:18:29 -0400

    first commit

---
+++ src/escapeTheTrolls25Weekly/EscapeTheTrolls.java
@@ -0,0 +1,12 @@
+package escapeTheTrolls25Weekly;
+
+public class EscapeTheTrolls {
+	public static void main(String[] args) {
+		EscapeTheTrolls game = new EscapeTheTrolls();
+		game.initiate();
+	}
+	
+	private void initiate() {
+		
+	}
+}
