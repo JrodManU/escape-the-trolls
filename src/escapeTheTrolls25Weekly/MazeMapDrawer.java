5d9f6080d76c70935fecf124c5cacd5caed43f13
JrodManU <jrodmanu@yahoo.com>
2016-09-06 09:52:34 -0400

    Intelij and Jframe for display

--- src/escapeTheTrolls25Weekly/MazeMapDrawer.java
+++ src/escapeTheTrolls25Weekly/MazeMapDrawer.java
@@ -1,14 +1,24 @@
 package escapeTheTrolls25Weekly;
 
-public class MazeMapDrawer {
-	
+import javax.swing.*;
+import java.awt.*;
+
+public class MazeMapDrawer extends JFrame {
+
+	private Font displayFont;
+
 	private EscapeTheTrolls game;
+	private JTextArea textArea;
 	
 	public MazeMapDrawer(EscapeTheTrolls game) {
 		this.game = game;
+		initializeTextArea();
 	}
-	
+
+
 	public void draw() {
+		textArea.setText("");
+		
 		for(int x = 0; x < game.getMazeMap().getMapSize().x; x++) {
 			String row = "";
 			for(int y = 0; y < game.getMazeMap().getMapSize().y; y++) {
@@ -20,6 +30,20 @@ public class MazeMapDrawer {
 				}
 			}
 			System.out.println(row);
+			textArea.append(row + "\n");
 		}
+	}
+
+	private void initializeTextArea() {
+		textArea = new JTextArea(game.getMazeMap().getMapSize().x, game.getMazeMap().getMapSize().y);
+		textArea.setEditable(false);
+
+		displayFont = new Font("monospaced", Font.PLAIN, 16);
+		textArea.setFont(displayFont);
+
+		setLayout(new GridLayout());
+		add(textArea);
+		pack();
+		setVisible(true);
 	}
 }
